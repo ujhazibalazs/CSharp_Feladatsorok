37e340442c7befafae138fe38c0e3e63675d980d
Ujházi Balázs <43323332+ujhazibalazs@users.noreply.github.com>
2018-11-20 19:37:09 +0100

    Add files via upload

---
+++ borton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace borton
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			bool[] cellak = new bool[100];
+			
+			//Kezdetben a cellák zárva vannak
+			for(int i=0;i<100;i++)
+			{
+				cellak[i] = false;
+			}
+			
+			//Játszik a rendőr
+			for(int j=0;j<100;j++)
+			{
+				for(int i=0;i<100;i++)
+				{
+					if((i+1) % (j+1) == 0)
+					{
+						if(cellak[i] == false)
+						{
+							cellak[i] = true;
+						}
+						else if(cellak[i] == true)
+						{
+							cellak[i] = false;
+						}
+					}
+				}
+			}
+			
+			//Kiiratás
+			for(int i=0;i<100;i++)
+			{
+				if(cellak[i] == true)
+				{
+					Console.WriteLine("{0}. Cella: nyitva.",i);
+				}
+				else if(cellak[i] == false)
+				{
+					Console.WriteLine("{0}. Cella: zárva.",i);
+				}
+			}
+			
+			//A szabaduló rabok kiírása
+			for(int i=0;i<100;i++)
+			{
+				if(cellak[i] == true)
+				{
+					Console.WriteLine("A {0}. cella rabja szabadul",i);
+				}
+			}
+			
+			Console.ReadKey(true);
+		}
+	}
+}
