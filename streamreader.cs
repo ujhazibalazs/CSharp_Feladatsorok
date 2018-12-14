43000d593853a90a97c683db50d1498d03bddbc9
Ujházi Balázs <43323332+ujhazibalazs@users.noreply.github.com>
2018-12-14 22:00:54 +0100

    Add files via upload

---
+++ streamreader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace io2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int szam;
+			int sor = 0;
+			
+			Console.WriteLine("Adj meg egy számot.");
+			szam = Convert.ToInt32(Console.ReadLine());
+			
+			StreamReader sr = new StreamReader("szavak.txt");
+			Console.WriteLine(sr.ReadLine());
+			sor++;
+			while(!sr.EndOfStream)
+			{
+				sr.ReadLine();
+				
+				if(sor%szam == 0)
+				{
+					Console.WriteLine(sr.ReadLine());
+				}
+				sor++;
+			}
+			
+			sr.Close();
+			
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
