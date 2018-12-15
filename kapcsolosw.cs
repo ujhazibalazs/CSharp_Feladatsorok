8c618f26021e909cee5eb46587ae73b284cc6dda
Ujházi Balázs <43323332+ujhazibalazs@users.noreply.github.com>
2018-12-15 08:40:28 +0100

    Add files via upload

---
+++ kapcsolosw.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace kapcsolo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			string szin = "";
+			
+			Console.WriteLine("Adj meg egy színt");
+			szin = Console.ReadLine();
+			
+			switch(szin)
+			{
+				case "piros":
+					Console.WriteLine("A megadott szín piros.");
+					break;
+				case "kék":
+					Console.WriteLine("A megadott szín kék.");
+					break;
+				case "zöld":
+					Console.WriteLine("A megadott szín zöld.");
+					break;
+				default:
+					Console.WriteLine("A megadott szín ismeretlen.");
+					break;
+			}
+			
+			StreamWriter sw = new StreamWriter("szinek.txt",true);
+			sw.WriteLine(szin);
+			sw.Close();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
